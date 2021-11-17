130c8af881b95fc7377a02bc0c9e00c675848160
ArquivosLara <larafabiacarvalho20@gmail.com>
2021-11-17 19:31:56 -0300

    Novo Arquivo

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+console.error('Essa é uma mensagem de erro');
+console.warn('Essa é uma mensagem de aviso!');
+
+// let, const
+console.clear();
+let idade = 19;
+const nome = 'Lara';
+console.log(nome);
+console.log(idade);
+idade = 19;
+
+
